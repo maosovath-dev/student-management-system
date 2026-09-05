44ecbb50612914051e46fb0d877781a102066f65
Mao Sovath <maosovath.kh@gmail.com>
2026-09-05 21:28:52 +0700

    update database

--- backend/src/models/student.model.js
+++ backend/src/models/student.model.js
@@ -1,41 +1,95 @@
 const pool = require("../config/db");
 
 const getAllStudents = async () => {
-    const [rows] = await pool.query(`SELECT * FROM students ORDER BY id DESC `);
-
-    return rows;
+  const [rows] = await pool.query(` SELECT * FROM students ORDER BY id DESC `);
+  return rows;
 };
 
 const findStudentById = async (studentCode) => {
-    const [rows] = await pool.query(` SELECT * FROM students WHERE student_code = ? `,
-        [studentCode],);
-
-    return rows;
+  const [rows] = await pool.query(` SELECT * FROM students WHERE id = ? `, [
+    id,
+  ]);
+  return rows;
 };
 
+const findStudentByCode = async (studentCode) => {
+  const [rows] = await pool.query(
+    ` SELECT * FROM students WHERE student_code = ? `,
+    [studentCode],
+  );
+  return rows;
+};
+
+// ==== Create Student
+
 const createStudent = async (body) => {
-    const arr = [
-        body.user_id,
-        body.student_code,
-        body.first_name,
-        body.last_name,
-        body.gender,
-        body.date_of_birth,
-        body.phone,
-        body.email,
-        body.address,
-        body.photo_url,
-        body.status,
-    ];
-    const [result] = await pool.query(
-        `INSERT INTO students (user_id, student_code, first_name, last_name, gender, date_of_birth, phone_number, email, address, photo_url, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-        arr
-    );
-    return result.insertId;
+  const arr = [
+    body.user_id || null,
+    body.student_code,
+    body.first_name,
+    body.last_name,
+    body.gender,
+    body.date_of_birth || null,
+    body.phone || null,
+    body.address || null,
+  ];
+  
+  const [result] = await pool.query(
+    ` INSERT INTO students ( user_id,
+        student_code,
+        first_name,
+        last_name,
+        gender,
+        date_of_birth,
+        phone,
+        address )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?) `,
+    arr,);
+  return result.insertId;
+};
+
+// ============= Update Student
+const updateStudent = async (id, body) => {
+  const arr = [
+    body.student_code,
+    body.first_name,
+    body.last_name,
+    body.gender,
+    body.date_of_birth || null,
+    body.phone || null,
+    body.address || null,
+    id,
+  ];
+
+  const [result] = await pool.query(` 
+    UPDATE students 
+    SET 
+        student_code = ?, 
+        first_name = ?,
+        last_name = ?,
+        gender = ?,
+        date_of_birth = ?,
+        phone = ?,
+        address = ?
+    WHERE id = ? 
+    `,arr,);
+  return result;
+};
+
+// ========= Delete Student
+
+const deleteStudent = async (id) => {
+  const [result] = await pool.query(` DELETE FROM students WHERE id = ? `, [
+    id,
+  ]);
+  return result;
 };
 
 module.exports = {
-    getAllStudents,
-    findStudentById,
-    createStudent,
+  getAllStudents,
+  findStudentById,
+  findStudentByCode,
+  createStudent,
+  updateStudent,
+  deleteStudent
 };
--- backend/src/services/student.service.js
+++ backend/src/services/student.service.js
@@ -17,26 +17,8 @@ const findStudentById = async (id) => {
     
 };
 
-const createStudent = async (data) => {
-    const {student_code, first_name, last_name, gender} = data;
-    if(!student_code || !first_name || !last_name || !gender){
-        const error = new Error('student_code, first_name, last_name and gender are required');
-        error.statusCode = 400;
-        throw error;
-    }
-    
-    const existingStudent = await studentModel.findStudentById(student_code);
-    if(existingStudent){
-        const error = new Error('student_code already exists');
-        error.statusCode = 400;
-        throw error;
-    }
-
-    
-};
 
 module.exports = {
     getAllStudents,
     findStudentById,
-    createStudent
 }
